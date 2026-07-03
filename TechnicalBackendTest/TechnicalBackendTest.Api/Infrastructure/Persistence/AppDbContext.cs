99805970bdc8ed7e5133b32b5a770e5a0429e072
xdevofc <xdevofc@gmail.com>
2026-07-03 00:20:10 -0300

    feat: agregando namespace

--- TechnicalBackendTest/TechnicalBackendTest.Api/Infrastructure/Persistence/AppDbContext.cs
+++ TechnicalBackendTest/TechnicalBackendTest.Api/Infrastructure/Persistence/AppDbContext.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 
+namespace TechnicalBackendTest.Api.Infrastructure.Persistence;
+
 public class AppDbContext: DbContext 
 {
     public AppDbContext(DbContextOptions<AppDbContext> options): base(options){}
--- TechnicalBackendTest/TechnicalBackendTest.Api/Program.cs
+++ TechnicalBackendTest/TechnicalBackendTest.Api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using TechnicalBackendTest.Api.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
